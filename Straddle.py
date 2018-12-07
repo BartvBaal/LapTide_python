d9a71431ac059a3525e99206191608e256629dda
frank <frnchambers@gmail.com>
2018-12-07 17:12:07 +0100

    added straddle software

---
+++ Straddle.py
@@ -0,0 +1,77 @@
+
+class lin:
+    @staticmethod
+    def inc (x, fact):
+        return x + fact
+    @staticmethod
+    def red (x, fact):
+        return x - fact
+class log:
+    @staticmethod
+    def inc (x, fact):
+        return x * fact
+    @staticmethod
+    def red (x, fact):
+        return x / fact
+
+class obs_empty_t:
+    def __call__ (self, point, itt):
+        pass
+class obs_print_t:
+    # in the case of outputing to file, could use somethings like this:
+    # def __init__ (self, output):
+    #     self.output = output
+    def __call__ (self, point, itt):
+        print "{} {} {}".format(itt, point[0], point[1])
+
+class straddle_t:
+
+    def __init__ (self, fn, fact, N_steps, root=None):
+        self.fn = fn
+        self.fact = fact
+        self.N_steps = N_steps
+        if root is None:
+            self.root = 0.0
+        else:
+            self.root = root
+
+    def guess (self, x):
+        return [x, self.fn(x)]
+
+    def search_general (self, init_x, op, obs):
+        itt = 0
+        pt_inc = self.guess(init_x)
+        pt_red = self.guess(init_x)
+        f0 = self.guess(init_x)[1]
+        while itt < self.N_steps:
+            pt_inc = self.guess(op.inc(pt_inc[0], self.fact))
+            pt_red = self.guess(op.red(pt_red[0], self.fact))
+            obs(pt_inc, itt)
+            obs(pt_red, itt)
+            if pt_inc[1]*f0 < 0: 
+                return [init_x, pt_inc[0]]
+            elif pt_red[1]*f0 < 0:
+                return [pt_red[0], init_x]
+            itt += 1
+
+    def search_lin (self, init_x, verbose=False):
+        if verbose is True:
+            return self.search_general(init_x, lin, obs_print_t())
+        else:
+            return self.search_general(init_x, lin, obs_empty_t())
+
+    def search_log (self, init_x, verbose=False):
+        if verbose is True:
+            return self.search_general(init_x, log, obs_print_t())
+        else:
+            return self.search_general(init_x, log, obs_empty_t())
+
+def search_lin (fn, init_x, fact, N_steps, verbose=False):
+    straddle = straddle_t(fn, fact, N_steps)
+    return straddle.search_lin(init_x, verbose)
+
+def search_log (fn, init_x, fact, N_steps, verbose=False):
+    straddle = straddle_t(fn, fact, N_steps)
+    return straddle.search_log(init_x, verbose)
+
+
---
+++ run_straddle.py
@@ -0,0 +1,53 @@
+#!/bin/bin/python
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from Straddle import straddle_t
+from Straddle import obs_print_t
+
+
+class polynomial_t:
+    def __init__ (self, coeffs):
+        self.coeffs = coeffs
+
+    def __call__ (self, x):
+        f  = 0.0
+        xp = 1.0
+        for ci in self.coeffs:
+            f  += ci * xp
+            xp *= x
+        return f
+
+
+def main ():
+
+    fn = polynomial_t([-4.0, 2.0, -1.5, 1.0])
+
+    x_grid = np.linspace(-2, 3, 50)
+    f_grid = fn(x_grid)
+
+    inc = 1.0
+    N_steps = 100
+    straddle = straddle_t(fn, inc, N_steps)
+
+    x0 = -1.0
+    verbose = True
+    print "Straddling to find bisection:"
+    bisec = straddle.search_lin(x0, verbose)
+
+    print "root in range: {}".format(bisec)
+
+    plt.plot(x_grid, f_grid)
+    plt.plot(bisec, [0, 0], '-o')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
+

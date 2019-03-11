e933d00591d0926f538ff48e56ba785e6e2d1713
BartvBaal <bartv.baal@gmail.com>
2019-03-11 12:53:01 +0100

    added in additional calculations and stuff

--- helpers/morsink_radius.py
+++ helpers/morsink_radius.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def radius(r_eq, mass, period, angle):
+def calc_radius_07(r_eq, mass, period, angle):
     """
     Calculates the radius at the angle, for the given parameters
     r_eq should be given in meters, and is the equatorial radius
@@ -20,7 +20,62 @@ def radius(r_eq, mass, period, angle):
     factor = 1 + a0*p0 + a2*p2 + a4*p4
     return factor
 
+
+def calc_radius_14(r_eq, mass, period, angle):
+    """
+    Uses new functions to calculate ellipticity.
+    Parameters the same as for calc_radius_07
+    """
+    GM = 6.67408e-11 * mass
+    x = GM / (r_eq * 299792458*299792458) 
+    om_bar_sq = 4*np.pi*np.pi * r_eq**3 / (period*period * GM)
+    o20 = -.788
+    o21 = 1.03 * x
+    factor = 1 + om_bar_sq * (o20 + o21) * np.cos(angle)**2
+    return factor
+
+
+def test_calc(r_eq, mass, freq, angle):
+    """
+    with G=c=1 so units don't match => values are garbage
+    """
+    x = mass / (r_eq)
+    angvel = (2*np.pi*r_eq * freq)**2
+    om_bar_sq = angvel * r_eq**3 / (mass)
+    o20 = -.788
+    o21 = 1.03 * x
+    factor = 1 + om_bar_sq * (o20 + o21) * np.cos(angle)**2
+    print "values", angvel, om_bar_sq, x
+    return factor
+
 if __name__ == "__main__":
-    angles = np.radians([90, 75, 62.5, 52.5, 45, 37.5, 27.5, 15, 0])
-    polar = radius(10000, 1.5*1.989e30, 1./600, angles)
-    print polar, 1-polar
+    r_eq = 12000  # in meters
+    mass = 2.2*1.989e30  # in kg
+    period = 1./600
+    degrees = [90, 75, 62.5, 52.5, 45, 37.5, 27.5, 15, 0]
+    angles = np.radians(degrees)
+    polar = calc_radius_07(r_eq, mass, period, angles)
+    data = zip(r_eq*polar*1e-3, 1-polar, degrees)
+    print "Morsink et al 2007 calculations:"
+    for elem in data:
+        print u"Radius of the star: {:.2f} km,\tdiff: {:.5f},\tco-latitudinal angle: {}\u00b0".format(*elem)
+    print "Ellipciticy: {}, e: {}".format(1 - polar[-1]/polar[0], 1 - (polar[-1]/polar[0])**2)
+
+    print "\n ======================================== \n"
+    newpolar = calc_radius_14(r_eq, mass, period, angles)
+    newdata = zip(r_eq*newpolar*1e-3, 1-newpolar, degrees)
+    print "AlGendy & Morsink 2014 calculations:"
+    for elem in newdata:
+        print u"Radius of the star: {:.2f} km,\tdiff: {:.5f},\tco-latitudinal angle: {}\u00b0".format(*elem)
+    print "Ellipciticy: {}, e: {}".format(1 - newpolar[-1]/newpolar[0], 1 - (newpolar[-1]/newpolar[0])**2)
+
+#    print "\n ======================================== \n"
+#    newr = 10.
+#    newpolar = test_calc(newr, 1.4, 600., angles)
+#    newdata = zip(newr*newpolar, 1-newpolar, degrees)
+#    for elem in newdata:
+#        print u"Radius of the star: {:.2f} km,\tdiff: {:.5f},\tco-latitudinal angle: {}\u00b0".format(*elem)
+#    print "Ellipciticy: {}".format(1 - newpolar[-1]/newpolar[0])
+
+
+

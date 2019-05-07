57fd341a4de62de0bc1a65fe3571bf214d797119
BartvBaal <bartv.baal@gmail.com>
2019-05-07 14:54:15 +0200

    Fixed mistakes in r-modes

--- helpers/Curvilinear_asymptotes.py
+++ helpers/Curvilinear_asymptotes.py
@@ -14,9 +14,9 @@ def r_modes(m, k, q, ecc=0, chi=0):
         if q == 0:
             raise ValueError("Invalid parameter; q has to be non-zero")
     elif type(q) == np.ndarray:
-        if m*q[m*q <= m*m]:
+        if np.any(m*q[m*q <= m*m]):
             raise ValueError("Invalid parameters; all m*q must be greater than m*m")
-        if q[q == 0.]:
+        if np.any(q[q == 0.]):
             raise ValueError("Invalid parameter; all q have to be non-zero")
     if type(m) != int or type(k) != int:
         raise TypeError("Input parameters m and k should be given as integers")
@@ -27,13 +27,35 @@ def r_modes(m, k, q, ecc=0, chi=0):
     s = -k-1
     eccdivsigsq = ecc / (sigsq)
     Gamma = 2.*chi
-    Upsilon = eccdivsigsq*m*q - m*q*Gamma - eccdivsigsq*Gamma
+    # Old math, E=0 approx
+#    Upsilon = eccdivsigsq*m*q - m*q*Gamma - eccdivsigsq*Gamma
 
-    n1 = (m*q - m*m*sigsq - Gamma)**2.
-    d1 = q*q * ((2.*s + 1)**2)
-    n2 = Upsilon / (q*q)
-    n3 = 2*n2 * sigsq * (m*q - m*m*sigsq + Gamma)
-    approx = (n1 / d1) - (n2) + (n3 / d1)
+#    n1 = (m*q - m*m*sigsq - Gamma)**2.
+#    d1 = q*q * ((2.*s + 1)**2)
+#    n2 = Upsilon / (q*q)
+#    n3 = 2*n2 * sigsq * (m*q - m*m*sigsq + Gamma)
+#    approx = (n1 / d1) - (n2) + (n3 / d1)
+
+#    M = -4*(m*q/sigsq - m*m - Gamma/sigsq)
+#    Psi = 2*(Gamma/sigsq + ecc/(sigsq*sigsq))
+#    d1 = 16*(q*q * (2*s + 1)**2)
+#    approx = ((M+Psi)**2 * sigsq*sigsq) / d1
+
+#    Q = (2.*s + 1) * q
+#    E = 1./ sigsq
+#    M = -4*(m*q/sigsq - m*m - Gamma/sigsq)
+#    Psi = 2*(Gamma/sigsq + ecc/(sigsq*sigsq))
+#    Upsilon = eccdivsigsq*m*q - m*q*Gamma - eccdivsigsq*Gamma  # I feel like this should be -Upsilon but I cant find where I would have made the mistake
+#    n1 = - ((Upsilon) / (q*q) + (Psi*Psi)/(16.*q*q*E*E))
+#    n2 = Q*Q*E*E/16.
+#    n3 = (M + Psi)/(E*E*Q*Q) - (4.*Upsilon)/(E*E*q*q*Q*Q) - (Psi*Psi)/(4.*q*q*E*E*E*E*Q*Q)
+#    approx = n1 + n2*(n3**2)
+
+    n1 = (m*q - m*m*sigsq)**2 - (m*q - m*m*sigsq)*(5.*chi + eccdivsigsq/2.) + chi*(6.*chi + eccdivsigsq)
+    d1 = q*q * (2.*s + 1)**2
+#    so = -m*eccdivsigsq/q + 2.*chi*m/q + 2.*chi*eccdivsigsq / (q*q)
+#    to = 4*sigsq*sigsq/(q*q) * (2.*chi/sigsq + eccdivsigsq**2)**2
+    approx = n1/d1 #+ so + to
     return approx
 
 
@@ -58,7 +80,8 @@ def g_modes(m, k, q, ecc=0, chi=0):
 
     fo = q*q * ((2.*s + 1)**2) / (sigsq**2)
     so = -2. * (m*q/sigsq - m*m - Gamma/sigsq)
-    to = eccdivsigsq*m/q - Gamma*m/q - Gamma*eccdivsigsq/(q*q)
+#    to = eccdivsigsq*m/q - Gamma*m/q - Gamma*eccdivsigsq/(q*q)  # Old math, E=0 approximation
+    to = - (Gamma/sigsq + ecc / (sigsq*sigsq))
     approx = fo + so + to
     return approx
 
